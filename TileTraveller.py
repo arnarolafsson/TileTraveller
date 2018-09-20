dffb885060e117f747e4f18dae7ed182628929dc
Arnar Ólafsson <arnarolafsson@Arnars-MacBook-Pro.local>
2018-09-20 12:25:59 +0000

    laga if og else setningar

--- TileTraveller.py
+++ TileTraveller.py
@@ -9,78 +9,31 @@ while position != 3.1:
         print("You can travel:", att)
         direction = input("Direction: ").lower()
 
-    if (x == 2 and y == 1):
-        print("You can travel: (N) orth.")
-        direct = input("Direction: ").lower()
-        if direct == "n":
-            y = y + 1
+    if direction == "n":
+        if position == 1.1 or position == 1.2 or position == 3.2 or position == 2.1:
+            position = position + 0.1
         else:
             print("Not a valid direction!")
-
-    if (x == 3 and y == 1):
-        print("Victory!")
-        break
-    if (x == 1 and y == 2):
-        print("You can travel: (N) orth or (E) ast or (S) outh")
-        direct = input("Direction: ").lower()
-        if direct == "n":
-            y = y + 1
-        elif direct == "e":
-            x = x + 1
-        elif direct == "s":
-            y = y - 1
+            villa = villa + 1
+    elif direction == "s":
+        if position == 1.2 or position == 1.3 or position == 2.2 or position == 3.3 or position == 3.2:
+            position = position - 0.1
         else:
             print("Not a valid direction!")
- 
-    if (x == 2 and y == 2):
-        print("You can travel: (S) outh or (W)est.")
-        direct = input("Direction: ").lower()
-        if direct == "s":
-            y = y - 1
-        elif direct == "w":
-            x = x - 1
+            villa = villa + 1
+    elif direction == "w":
+        if position == 2.3 or position == 2.2 or position == 3.3:
+            position = position - 1
         else:
             print("Not a valid direction!")
-
-    if (x == 3 and y == 2):
-        print("You can travel: (N) orth or (S) outh.")
-        direct = input("Direction: ").lower()
-        if direct == "n":
-            y = y + 1
-        elif direct == "s":
-            y = y - 1
+            villa = villa + 1
+    elif direction == "e":
+        if position == 1.2 or position == 1.3 or position == 2.3:
+            position = position + 1
         else:
             print("Not a valid direction!")
-
-    if (x == 1 and y == 3):
-        print("You can travel: (E) ast or (S) outh")
-        direct = input("Direction: ").lower()
-        if direct == "e":
-            x = x + 1
-        elif direct == "s":
-            y = y - 1
-        else:
-            print("Not a valid direction!")
-
-    if (x == 2 and y == 3):
-        print("You can travel: (E) ast or (W) est")
-        direct = input("Direction: ").lower()
-        if direct == "e":
-            x = x + 1
-        elif direct == "w":
-            x = x - 1
-        else:
-            print("Not a valid direction!")
-
-    if ( x == 3 and y == 3):
-        print("You can travel: (W) est or (S) outh")
-        direct = input("Direction: ").lower()
-        if direct == "w":
-            x = x - 1
-        elif direct == "s":
-            y = y - 1
-        else:
-            print("Not a valid direction!")
-
-    
-            
+            villa = villa + 1
+    else:
+        print("Not a valid direction!")
+        villa = villa + 1
+    position = round(position, 1)
